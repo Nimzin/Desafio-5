98b718c95e832faf90f7b8c8df48a383edf031c4
55229 <55229@DESKTOP-OTFAROA>
2024-11-01 12:15:11 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio 5/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string palavra = "Target Sistemas";      
+        string palavraInvertida = Inverter(palavra);
+
+
+        Console.WriteLine($"Palavra normal: {palavra}");
+        Console.WriteLine($"Palavra invertida: {palavraInvertida}");
+    }
+
+    public static string Inverter(string input) 
+    {
+        char [] chars = new char[input.Length];  //array de caracteres do mesmo tamanho da string
+
+        for (int i = 0; i <input.Length; i++) //percorrer a string de tras p frente.
+        {
+            chars[i] = input[input.Length - 1 -i]; //inversao do caractere
+        }
+        return new string(chars); //conversao do array em string
+    }
+}
